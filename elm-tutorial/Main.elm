c7d86052650545bec907bb3211143768785fd7ba
otofu-square <yukita@feedforce.jp>
2017-10-14 14:47:19 +0900

    :+1: Implement expand & collapse strings

--- elm-tutorial/Main.elm
+++ elm-tutorial/Main.elm
@@ -1,32 +1,54 @@
 module App exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Html exposing (Html, button, div, program, text)
+import Html.Events exposing (onClick)
+
+
+-- MODEL
 
 
 type alias Model =
-    String
+    Bool
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( "Hello World", Cmd.none )
+    ( False, Cmd.none )
+
+
+
+-- MESSAGES
 
 
 type Msg
-    = NoOp
+    = Expand
+    | Collapse
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ text model ]
+    if model then
+        div []
+            [ button [ onClick Collapse ] [ text "Collapse" ]
+            , text "Widget"
+            ]
+    else
+        div []
+            [ button [ onClick Expand ] [ text "Expand" ] ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        Expand ->
+            ( True, Cmd.none )
+
+        Collapse ->
+            ( False, Cmd.none )
 
 
 subscriptions : model -> Sub Msg
