8eafa0a709ef3875f4fa0fbd8ac7bb2479cee93a
otofu-square <yukita@feedforce.jp>
2017-10-23 20:13:45 +0900

    :+1: Implement player edit view

--- elm-tutorial-application/src/Update.elm
+++ elm-tutorial-application/src/Update.elm
@@ -2,6 +2,7 @@ module Update exposing (..)
 
 import Models exposing (..)
 import Msgs exposing (..)
+import Routing exposing (parseLocation)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -9,3 +10,10 @@ update msg model =
     case msg of
         OnFetchPlayers playerListWebData ->
             ( { model | players = playerListWebData }, Cmd.none )
+
+        OnLocationChange location ->
+            let
+                newRoute =
+                    parseLocation location
+            in
+            ( { model | route = newRoute }, Cmd.none )
--- elm-tutorial-application/src/View.elm
+++ elm-tutorial-application/src/View.elm
@@ -1,9 +1,11 @@
 module View exposing (..)
 
 import Html exposing (Html, div, text)
-import Msgs exposing (..)
 import Models exposing (..)
+import Msgs exposing (..)
+import Players.Edit
 import Players.List
+import RemoteData
 
 
 view : Model -> Html Msg
@@ -14,4 +16,45 @@ view model =
 
 page : Model -> Html Msg
 page model =
-    Players.List.view model.players
+    case model.route of
+        PlayersRoute ->
+            Players.List.view model.players
+
+        PlayerRoute playerId ->
+            playerEditPage model playerId
+
+        NotFoundRoute ->
+            notFoundView
+
+
+playerEditPage : Model -> PlayerId -> Html Msg
+playerEditPage model playerId =
+    case model.players of
+        RemoteData.NotAsked ->
+            text ""
+
+        RemoteData.Loading ->
+            text "Loading ..."
+
+        RemoteData.Success players ->
+            let
+                maybePlayer =
+                    players
+                        |> List.filter (\player -> player.id == playerId)
+                        |> List.head
+            in
+            case maybePlayer of
+                Just player ->
+                    Players.Edit.view player
+
+                Nothing ->
+                    notFoundView
+
+        RemoteData.Failure err ->
+            text (toString err)
+
+
+notFoundView : Html msg
+notFoundView =
+    div []
+        [ text "Not found" ]
