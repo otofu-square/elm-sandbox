5c9f0d2b187eadf8501be62d7a4bca8bb04dacc1
otofu-square <yukita@feedforce.jp>
2017-10-29 00:06:32 +0900

    :+1: Quote

--- elm-auth0-tutorial/src/Main.elm
+++ elm-auth0-tutorial/src/Main.elm
@@ -1,45 +1,8 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src)
-
-
----- MODEL ----
-
-
-type alias Model =
-    {}
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
-
-
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , div [] [ text "Your Elm App is working!" ]
-        ]
-
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
 ---- PROGRAM ----
@@ -53,3 +16,56 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { quote : String }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { quote = "" }, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = GetQuote
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GetQuote ->
+            ( { model | quote = model.quote ++ "A quote! " }, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ h2
+            [ class "text-center" ]
+            [ text "Chuck Norris Quotes" ]
+        , p
+            [ class "text-center" ]
+            [ button
+                [ class "btn btn-success", onClick GetQuote ]
+                [ text "Grab a quote!" ]
+            ]
+        , blockquote
+            []
+            [ p
+                []
+                [ text model.quote ]
+            ]
+        ]
